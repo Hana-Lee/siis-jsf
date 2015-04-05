28dbb3cb4ff69410192a694eef8a21534e558bd8
Hana Lee <voyaging@leehana.co.kr>
2015-04-06 02:59:06 +0900

    war 파일 생성을 위해 maven 설정 파일을 수정함

---
+++ src/test/java/model/BookTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Lee Hana on 2015-04-04 오전 7:42.
+ * @author Lee Hana
+ * @since 2015-04-04 오전 7:42
+ */
+public class BookTest {
+
+    @Test
+    public void testBookModel() {
+        System.out.println("Book Model Test") ;
+    }
+}
