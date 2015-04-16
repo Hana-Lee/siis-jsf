3862422ac2b48cf5982a92ca5118fb0722d175a9
Lee Hana <voyaging@leehana.co.kr>
2015-04-16 18:54:00 +0900

    불필요한 테스트 코드 제거

--- src/test/java/kr/co/leehana/siis/parser/HtmlParser.java
+++ src/test/java/kr/co/leehana/siis/parser/HtmlParser.java
@@ -53,155 +53,7 @@ public class HtmlParser {
     private final String searchText4 = "도서<br>상태";
 
     @Test
-    @Ignore
-    public void testHtmlParsing() throws UnsupportedEncodingException {
-        String baseText = "detailurl";
-        String testUrl1 = "http://115.84.165.14/EngineGateway/Detail.lsm?id=1428394645702&dbnum=42551&seq=9&attr=1&detailurl=http%3A%2F%2Flibrary.gangnam.go.kr%2Fsearch%2Fdetail%2FCATCAZ000000165430%3FmainLink%3D%2Fsearch%2Fcaz%26briefLink%3D%2Fsearch%2Fcaz%2Fresult%3Flmtsn%3D000000000003_A_lmtsn%3D000000000006_A_cpp%3D10_A_range%3D000000000021_A__inc%3Don_A__inc%3Don_A__inc%3Don_A__inc%3Don_A__inc%3Don_A__inc%3Don_A_lmt0%3DTOTAL_A_rf%3D_A_lmt1%3DTOTAL_A_si%3DTOTAL_A_si%3D2_A_si%3D3_A_weight2%3D0.5_A_weight0%3D0.5_A_weight1%3D0.5_A_inc%3DTOTAL_A_q%3D%25EB%258F%2584%25EA%25B0%2580%25EB%258B%2588_A_q%3D_A_q%3D_A_b0%3Dand_A_b1%3Dand_A_lmtst%3DOR_A_lmtst%3DOR_A_rt%3D_A_st%3DKWRD_A_msc%3D10000";
-        int detailUrlIndex = testUrl1.toLowerCase().indexOf(baseText);
-
-        String testUrl = StringUtils.EMPTY;
-        if (detailUrlIndex > -1) {
-            testUrl = URLDecoder.decode(testUrl1.substring(detailUrlIndex + baseText.length() + 1), "UTF-8");
-        } else {
-            testUrl = testUrl1;
-        }
-        logger.info(detailUrlIndex);
-        logger.info(testUrl);
-
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-        Future<Response> f = asyncHttpClient.prepareGet(testUrl).execute();
-
-        String htmlDocument = "";
-        try {
-            Response r = f.get();
-            htmlDocument = r.getResponseBody();
-            InputStream stream = r.getResponseBodyAsStream();
-            CharsetDetector detector = new CharsetDetector();
-            detector.setText(htmlDocument.getBytes());
-            for (CharsetMatch match : detector.detectAll()) {
-                logger.info(match.getName());
-            }
-        } catch (InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
-        }
-
-        Document dom = null;
-        if (StringUtils.isNotBlank(htmlDocument)) {
-            dom = Jsoup.parse(htmlDocument);
-        }
-
-        if (dom != null) {
-            Elements tableElems = dom.select("table");
-
-            for(Element tableElem : tableElems) {
-                boolean hasContent = tableElem.toString().contains(searchText3);
-                if (hasContent) {
-                    Elements tdElems = tableElem.select("tbody td");
-                    for (Element td : tdElems) {
-                        logger.info(td.toString());
-                    }
-                } else {
-                    logger.error("No data found");
-                }
-            }
-        }
-    }
-
-    @Test
-    @Ignore
-    public void testHtmlParsing2() {
-// String testUrl = "http://gnlib.sen.go.kr/Book-Info.do?rec_key=5179499254&st_code=KEY_5179499252&lib_code=111003&searchType=search-simple&boardId=GN_D01";
-        String testUrl = "http://www.gangbuklib.seoul.kr/youth/01.search/?m=0102&mode=v&RK=302871402&bGubn=M&LibCD=MB";
-
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-        Future<Response> f2 = asyncHttpClient.prepareGet(testUrl).execute();
-
-        String htmlDocument = "";
-        try {
-            Response r = f2.get();
-            htmlDocument = r.getResponseBody();
-            InputStream stream = r.getResponseBodyAsStream();
-            CharsetDetector detector = new CharsetDetector();
-            detector.setText(htmlDocument.getBytes());
-            for (CharsetMatch match : detector.detectAll()) {
-                logger.info(match.getName());
-            }
-            htmlDocument = StringUtils.toEncodedString(htmlDocument.getBytes("latin1"), Charset.forName("UTF-8"));
-        } catch (InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
-        }
-
-        Document dom = null;
-        if (StringUtils.isNotBlank(htmlDocument)) {
-            dom = Jsoup.parse(htmlDocument);
-        }
-
-        if (dom != null) {
-            Elements tableElems = dom.select("table");
-
-            for (Element tableElem : tableElems) {
-                boolean hasContent = tableElem.toString().contains(searchText4);
-                if (hasContent) {
-                    Elements tdElems = tableElem.select("tbody td");
-                    for (Element td : tdElems) {
-                        logger.info(td.toString());
-                    }
-                } else {
-                    logger.error("No data found");
-                }
-            }
-        }
-    }
-
-    @Test
-    @Ignore
-    public void testHtmlParsing3() {
-        String testUrl = "http://gnlib.sen.go.kr/Book-Info.do?rec_key=5179499254&st_code=KEY_5179499252&lib_code=111003&searchType=search-simple&boardId=GN_D01";
-// String testUrl = "http://www.gangbuklib.seoul.kr/youth/01.search/?m=0102&mode=v&RK=302871402&bGubn=M&LibCD=MB";
-
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-        Future<Response> f2 = asyncHttpClient.prepareGet(testUrl).execute();
-
-        String htmlDocument = "";
-        try {
-            Response r = f2.get();
-            htmlDocument = r.getResponseBody();
-            InputStream stream = r.getResponseBodyAsStream();
-            CharsetDetector detector = new CharsetDetector();
-            detector.setText(htmlDocument.getBytes());
-            for (CharsetMatch match : detector.detectAll()) {
-                logger.info(match.getName());
-            }
-// htmlDocument = StringUtils.toEncodedString(htmlDocument.getBytes("8859_1"), Charset.forName("UTF-8"));
-        } catch (InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
-        }
-
-        Document dom = null;
-        if (StringUtils.isNotBlank(htmlDocument)) {
-            dom = Jsoup.parse(htmlDocument);
-        }
-
-        if (dom != null) {
-            Elements tableElems = dom.select("table");
-
-            for (Element tableElem : tableElems) {
-                boolean hasContent = tableElem.toString().contains(searchText3);
-                if (hasContent) {
-                    Elements tdElems = tableElem.select("tbody td");
-                    for (Element td : tdElems) {
-                        logger.info(td.toString());
-                    }
-                } else {
-                    logger.error("No data found");
-                }
-            }
-        }
-    }
-
-    @Test
     public void testGetJsonData() throws IOException, SQLException, ClassNotFoundException {
-//        String searchWord = "%EB%8F%84%EA%B0%80%EB%8B%88";
         String searchWord = "마이크로코스모스";
         long startTime = System.currentTimeMillis();
         String searchUrlTemplate = "http://meta.seoul.go.kr/libstepsV5_seoul/ctrl/search.lsm?category1=%s&category2=&category3=&text1=%s&text2=&text3=&as1=&as2=&as3=&year1=&year2=&dbnum=%s&recstart=%s&display=%s&target=&op=&op2=&sort=&id=%s&skey=798&ckey=0&host=115.84.165.14&_=1428567948467";
@@ -217,7 +69,7 @@ public class HtmlParser {
         String categoryNumber = "1";
 
         int totalCount = 0;
-        ExecutorService excutor = Executors.newFixedThreadPool(6);
+        ExecutorService excutor = Executors.newFixedThreadPool(5);
         int count = 0;
         while (rs.next()) {
             int randomNumber = (int) ((Math.random() * 100) + 1);
@@ -230,19 +82,6 @@ public class HtmlParser {
             Runnable worker = new BookSearcher(searchUrl, libraryCode);
 
             excutor.execute(worker);
-
-//            if (count == 20) {
-//                excutor.shutdown();
-//
-//                while (!excutor.isTerminated()){
-//                }
-//
-//                excutor = Executors.newFixedThreadPool(11);
-//                count = 0;
-//            } else {
-//                count++;
-//            }
-
         }
 
         rs.close();
