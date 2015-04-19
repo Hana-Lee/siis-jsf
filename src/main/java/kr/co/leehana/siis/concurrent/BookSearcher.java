196a4c1408fa9776027ac354b572c2f82a1f1aab
Hana Lee <voyaging@leehana.co.kr>
2015-04-20 01:54:58 +0900

    link href 연결 시 decode 되지 않아 도서 정보 URL을 URLEncode 하지 않고 원본 그대로 저장하도록 변경.

--- src/main/java/kr/co/leehana/siis/concurrent/BookSearcher.java
+++ src/main/java/kr/co/leehana/siis/concurrent/BookSearcher.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
