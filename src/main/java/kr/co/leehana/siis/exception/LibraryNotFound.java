dff45784f3148eb5ffe88b49f7fd97a1524273e4
Lee Hana <voyaging@leehana.co.kr>
2015-04-22 15:26:45 +0900

    Service 클래스에서 사용할 IOException 을 확장한 각 모델용 Exception 클래스 생성.

---
+++ src/main/java/kr/co/leehana/siis/exception/LibraryNotFound.java
@@ -0,0 +1,12 @@
+package kr.co.leehana.siis.exception;
+
+import java.io.IOException;
+
+/**
+ * Created by Lee Hana on 2015-04-22 오후 3:24.
+ *
+ * @author Lee Hana
+ */
+public class LibraryNotFound extends IOException {
+	private static final long serialVersionUID = -6937914471890502468L;
+}
