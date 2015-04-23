610fd211aae457ffa8968ca80ba6545049602faf
Lee Hana <voyaging@leehana.co.kr>
2015-04-23 18:19:58 +0900

    검색 히스토리 모델 테스트 개발 완료.

--- src/test/java/kr/co/leehana/siis/model/SearchHistoryTest.java
+++ src/test/java/kr/co/leehana/siis/model/SearchHistoryTest.java
@@ -5,21 +5,22 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import kr.co.leehana.siis.config.WebAppConfig;
+import kr.co.leehana.siis.config.WebAppConfigDevProfile;
 import kr.co.leehana.siis.service.LibraryService;
 import kr.co.leehana.siis.service.SearchHistoryService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -28,7 +29,8 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Lee Hana
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = WebAppConfig.class)
+@ContextConfiguration(classes = WebAppConfigDevProfile.class, loader = AnnotationConfigContextLoader.class)
+@ActiveProfiles(value = "dev")
 public class SearchHistoryTest {
 
 	private final Log log = LogFactory.getLog(getClass());
@@ -39,12 +41,26 @@ public class SearchHistoryTest {
 	@Autowired
 	private LibraryService libraryService;
 
-	private List<Book> bookList = new ArrayList<>();
+	private List<String> wordList = Arrays
+			.asList("토비의 스프링3", "괴델,에셔,바흐", "고미숙");
 
-	@Before
-	public void setUp() throws Exception {
+	private final String testSearchWord = "토비의 스프링 3";
 
+	@Test
+	@Transactional
+	public void testCreateSearchHistoryWithNewBookList() throws Exception {
+		SearchHistory history = new SearchHistory();
+		history.setSearchWord(convertWhiteSpacesToUnderscores(testSearchWord));
+		history.setBooks(makeDummyBookList());
+
+		searchHistoryService.create(history);
+
+		assertThat(history.getId(), is(4L));
+	}
+
+	private List<Book> makeDummyBookList() throws Exception {
 		Library library = libraryService.findById("1791");
+		List<Book> newBookList = new ArrayList<>();
 
 		Book dummyBook1 = new Book();
 		dummyBook1.setTitle("토비의 스프링3");
@@ -55,7 +71,7 @@ public class SearchHistoryTest {
 		dummyBook1.setLibrary(library);
 		dummyBook1.setCreated(new Date(System.currentTimeMillis()));
 
-		bookList.add(dummyBook1);
+		newBookList.add(dummyBook1);
 
 		Book dummyBook2 = new Book();
 		dummyBook2.setTitle("토비의 스프링3.1");
@@ -66,7 +82,7 @@ public class SearchHistoryTest {
 		dummyBook2.setLibrary(library);
 		dummyBook2.setCreated(new Date(System.currentTimeMillis()));
 
-		bookList.add(dummyBook2);
+		newBookList.add(dummyBook2);
 
 		Book dummyBook3 = new Book();
 		dummyBook3.setTitle("토비의 스프링 3.1 시작부터 끝까지");
@@ -77,35 +93,40 @@ public class SearchHistoryTest {
 		dummyBook3.setLibrary(library);
 		dummyBook3.setCreated(new Date(System.currentTimeMillis()));
 
-		bookList.add(dummyBook3);
+		newBookList.add(dummyBook3);
 
-		SearchHistory history = new SearchHistory();
-		history.setSearchWord(convertWhiteSpacesToUnderscores("클린 코드"));
-		history.setBooks(bookList);
-
-		searchHistoryService.create(history);
-	}
-
-	@Test
-	@Transactional
-	public void testCreateSearchHistoryWithNewBookList() {
-		SearchHistory history = new SearchHistory();
-		history.setSearchWord(convertWhiteSpacesToUnderscores("토비의 스프링3"));
-		history.setBooks(bookList);
-
-		searchHistoryService.create(history);
-
-		assertThat(history.getId(), is(3L));
+		return newBookList;
 	}
 
 	@Test
 	@Transactional
 	public void testGetSearchHistoryWithSearchWord() {
-		String searchWord = "클린_코드";
-		SearchHistory oldHistory = searchHistoryService
-				.findBySearchWord(convertWhiteSpacesToUnderscores(searchWord));
-		assertThat(oldHistory, is(not(nullValue(SearchHistory.class))));
-		assertThat(oldHistory.getSearchWord(), is(searchWord));
+		for (String searchWord : wordList) {
+			SearchHistory history = searchHistoryService
+					.findBySearchWord(convertWhiteSpacesToUnderscores(searchWord));
+			assertThat(history, is(not(nullValue(SearchHistory.class))));
+		}
+	}
+
+	@Test
+	public void testConvertWhiteSpacesToUnderscores() {
+		String convertedSearchWord = convertWhiteSpacesToUnderscores(testSearchWord);
+		assertThat(convertedSearchWord, is("토비의_스프링_3"));
+	}
+
+	@Test
+	@Transactional
+	public void testGetBooksFromSearchHistory() {
+		for (String searchWord : wordList) {
+			SearchHistory history = searchHistoryService
+					.findBySearchWord(convertWhiteSpacesToUnderscores(searchWord));
+			assertThat(history, is(not(nullValue(SearchHistory.class))));
+
+			List<Book> books = history.getBooks();
+
+			assertThat(books, is(not(nullValue(List.class))));
+			assertThat(books.size(), is(3));
+		}
 	}
 
 	private String convertWhiteSpacesToUnderscores(String searchWord) {
