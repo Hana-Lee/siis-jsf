e4ce23ca3d4c9ecac5f2201b4a9a83611f9e0d0c
Hana Lee <voyaging@leehana.co.kr>
2015-04-24 07:41:46 +0900

    모델 테스트 슈트 생성.

---
+++ src/test/java/kr/co/leehana/siis/model/ModelTestSuite.java
@@ -0,0 +1,16 @@
+package kr.co.leehana.siis.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Lee Hana on 2015-04-24 오전 7:26.
+ *
+ * @author Lee Hana
+ * @since 2015-04-24 오전 7:26
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BookTest.class, LibraryTest.class, SearchHistoryTest.class })
+public class ModelTestSuite {
+}
