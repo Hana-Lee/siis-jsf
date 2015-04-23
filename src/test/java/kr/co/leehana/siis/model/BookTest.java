823f54bb6c4bf1861cc8cc119f425aa1a7df42c1
Lee Hana <voyaging@leehana.co.kr>
2015-04-23 18:54:35 +0900

    도서 모델 테스트 개발 완료.

--- src/test/java/kr/co/leehana/siis/model/BookTest.java
+++ src/test/java/kr/co/leehana/siis/model/BookTest.java
@@ -1,20 +1,25 @@
 package kr.co.leehana.siis.model;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+
 import java.sql.Date;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
-import kr.co.leehana.siis.config.WebAppConfig;
+import kr.co.leehana.siis.config.WebAppConfigDevProfile;
+import kr.co.leehana.siis.exception.BookNotFound;
 import kr.co.leehana.siis.service.BookService;
 import kr.co.leehana.siis.service.LibraryService;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Lee Hana on 2015-04-04 오전 7:42.
@@ -23,7 +28,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @since 2015-04-04 오전 7:42
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = WebAppConfig.class)
+@ContextConfiguration(classes = WebAppConfigDevProfile.class)
+@ActiveProfiles(value = "dev")
 public class BookTest {
 
 	@Autowired
@@ -48,21 +54,58 @@ public class BookTest {
 
 		bookService.create(newBook);
 
-		List<Book> oldBooks = bookService.findAll();
+		assertThat(newBook.getId(), is(10L));
 
-		Assert.assertEquals(1L, newBook.getId());
+		List<Book> allBooks = bookService.findAll();
 
-		for (Book book : oldBooks) {
-			System.out.println("new book id : " + book.getId());
-		}
-
-		List<Book> books2 = newLibrary.getBooks();
-		for (Book book : books2) {
-			System.out.println("old book id : " + book.getId());
-		}
+		assertThat(allBooks, is(not(nullValue(List.class))));
+		assertThat(allBooks.size(), is(10));
 	}
 
 	@Test
-	public void testGetBookBySearchWord() {
+	public void testDeleteBook() throws BookNotFound {
+		Book book = bookService.findById(1L);
+
+		assertThat(book, is(not(nullValue(Book.class))));
+
+		bookService.delete(book.getId());
+
+		List<Book> books = bookService.findAll();
+
+		assertThat(books, is(not(nullValue(List.class))));
+		assertThat(books.size(), is(8));
+	}
+
+	@Test(expected = BookNotFound.class)
+	public void testDeleteBookWithBookNotFoundException() throws BookNotFound {
+		bookService.delete(10L);
+	}
+
+	@Test
+	public void testUpdateBook() throws BookNotFound {
+		String newAuthor = "이하나";
+		Book book = bookService.findById(1L);
+		book.setAuthor(newAuthor);
+		bookService.update(book);
+
+		assertThat(book.getAuthor(), is(newAuthor));
+	}
+
+	@Test(expected = BookNotFound.class)
+	public void testUpdateBookWithBookNotFoundException() throws BookNotFound {
+		Book book = bookService.findById(1L);
+		book.setId(10L);
+
+		bookService.update(book);
+	}
+
+	@Test
+	@Transactional
+	public void testGetLibraryFromBook() {
+		Book book = bookService.findById(1L);
+		Library library = book.getLibrary();
+
+		assertThat(library, is(not(nullValue(Library.class))));
+		assertThat(library.getCode(), is("1081"));
 	}
 }
