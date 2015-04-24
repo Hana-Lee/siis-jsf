4dd3653474641bde5281ca8dbbf93773b5465057
Lee Hana <voyaging@leehana.co.kr>
2015-04-24 15:31:39 +0900

    Model 테스트보다 Service 테스트가 맥락상 더 맞다고 판단하여 폴더 및 테스트 클래스 이름 변경.

--- src/test/java/kr/co/leehana/siis/service/LibraryServiceTest.java
+++ src/test/java/kr/co/leehana/siis/service/LibraryServiceTest.java
@@ -1,4 +1,4 @@
-package kr.co.leehana.siis.model;
+package kr.co.leehana.siis.service;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import kr.co.leehana.siis.config.WebAppConfigDevProfile;
+import kr.co.leehana.siis.model.Book;
+import kr.co.leehana.siis.model.Library;
 import kr.co.leehana.siis.service.LibraryService;
 
 import org.apache.commons.logging.Log;
@@ -28,7 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = WebAppConfigDevProfile.class)
 @ActiveProfiles(value = "dev")
-public class LibraryTest {
+public class LibraryServiceTest {
 
 	private final Log log = LogFactory.getLog(getClass());
 
