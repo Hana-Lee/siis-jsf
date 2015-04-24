10476a52143f11225e4462597f2a4dc6f96636c1
Lee Hana <voyaging@leehana.co.kr>
2015-04-24 15:30:49 +0900

    불필요한 import 정리

--- src/main/java/kr/co/leehana/siis/service/SearchHistoryService.java
+++ src/main/java/kr/co/leehana/siis/service/SearchHistoryService.java
@@ -1,8 +1,5 @@
 package kr.co.leehana.siis.service;
 
-import java.util.List;
-
-import kr.co.leehana.siis.model.Book;
 import kr.co.leehana.siis.model.SearchHistory;
 
 public interface SearchHistoryService {
