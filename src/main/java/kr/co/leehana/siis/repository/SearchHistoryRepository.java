27a44da2fbb612e0b8ac6138758ca2eefed7778f
Lee Hana <voyaging@leehana.co.kr>
2015-04-24 15:30:49 +0900

    불필요한 import 정리

--- src/main/java/kr/co/leehana/siis/repository/SearchHistoryRepository.java
+++ src/main/java/kr/co/leehana/siis/repository/SearchHistoryRepository.java
@@ -4,8 +4,6 @@ import kr.co.leehana.siis.model.SearchHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 /**
  * Created by Lee Hana on 2015-04-21 오후 5:17.
  *
