da841ff8f495aff0b40bb626db34002f94943cfa
Lee Hana <voyaging@leehana.co.kr>
2015-04-27 14:25:28 +0900

    Helper 클래스들의 테스트 Suite 생성.

---
+++ src/test/java/kr/co/leehana/siis/helper/HelperTestSuite.java
@@ -0,0 +1,15 @@
+package kr.co.leehana.siis.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Lee Hana on 2015-04-27 오후 2:13.
+ *
+ * @author Lee Hana
+ * @since 2015-04-27 오후 2:13
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ StringHelperTest.class, UserAgentTest.class })
+public class HelperTestSuite {
+}
