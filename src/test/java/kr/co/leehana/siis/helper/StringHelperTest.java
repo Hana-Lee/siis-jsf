a067ff04bf3418bf182fef38ab8ac2b173180191
Lee Hana <voyaging@leehana.co.kr>
2015-04-27 14:25:08 +0900

    StringHelper 유틸 클래스 테스트 커버리지 100% 완료.

--- src/test/java/kr/co/leehana/siis/helper/StringHelperTest.java
+++ src/test/java/kr/co/leehana/siis/helper/StringHelperTest.java
@@ -1,6 +1,6 @@
 package kr.co.leehana.siis.helper;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
@@ -11,6 +11,12 @@ import org.junit.Test;
  * @author Lee Hana
  */
 public class StringHelperTest {
+
+	@Test
+	public void testCreateStringHelperObject() {
+		StringHelper helper = new StringHelper();
+		assertThat(helper, is(not(nullValue(StringHelper.class))));
+	}
 
 	@Test
 	public void testConvertWhiteSpacesToUnderscores() {
@@ -36,5 +42,23 @@ public class StringHelperTest {
 		source = " 토비의  스프링 3.1";
 		convertedSource = StringHelper.convertWhiteSpacesToUnderscores(source);
 		assertThat(convertedSource, is(expect));
+	}
+
+	@Test
+	public void testBlankStringConvert() {
+		String blankString = "";
+		String convertedString = StringHelper
+				.convertWhiteSpacesToUnderscores(blankString);
+
+		assertThat(convertedString, is(blankString));
+	}
+
+	@Test
+	public void testNonWhitespaceConvert() {
+		String nonWhitespaceString = "이하나";
+		String convertedString = StringHelper
+				.convertWhiteSpacesToUnderscores(nonWhitespaceString);
+
+		assertThat(convertedString, is(nonWhitespaceString));
 	}
 }
